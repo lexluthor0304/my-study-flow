33eb21d6fa07704e3aa6e0b6b77ed29887073838
lex <lexluthor0304@gmail.com>
2018-09-03 19:00:23 +0900

    update

--- java/ERP/src/helloWorld/HelloWorld.java
+++ java/ERP/src/helloWorld/HelloWorld.java
@@ -1,10 +1,28 @@
 package helloWorld;
 
-public class HelloWorld {
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class HelloWorld extends HttpServlet{
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws IOException, ServletException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello World!</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello World</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
 	}
-
 }
