051709ce11e77112687756b847b73e26cb941078
lex <lexluthor0304@gmail.com>
2018-09-10 15:17:36 +0900

    study thymeleaf

--- Springboot/GradlePJ/src/main/java/com/study/SpringBootApplication/hello/HelloGradle.java
+++ Springboot/GradlePJ/src/main/java/com/study/SpringBootApplication/hello/HelloGradle.java
@@ -1,6 +1,5 @@
 package com.study.SpringBootApplication.hello;
 
-import org.assertj.core.data.Index;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
