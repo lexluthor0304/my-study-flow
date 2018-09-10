18b60dad522fe713a60b67084c112f72d80ff1ae
lex <lexluthor0304@gmail.com>
2018-09-10 18:30:31 +0900

    study thymeleaf

---
+++ Springboot/Study3/src/main/java/com/study/SpringBootApplication/forward/Forward.java
@@ -0,0 +1,27 @@
+package com.study.SpringBootApplication.forward;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Forward {
+
+	 @RequestMapping(value="/")
+	 public ModelAndView index(ModelAndView mav) {
+		 mav.setViewName("index");
+		 return mav;
+	 }
+	 
+	 @RequestMapping(value="/other")
+	 public ModelAndView other(ModelAndView mav1) {
+		 mav1.setViewName("redirect:/");
+		 return mav1;
+	 }
+	 
+	 @RequestMapping(value="/home")
+	 public ModelAndView home(ModelAndView mav2) {
+		 mav2.setViewName("forward:/");
+		 return mav2;
+	 }
+}
